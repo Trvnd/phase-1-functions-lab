bdaa7fe3b18de35831818d0d662b2f8d226f45ac
Jimmy Senecal <jamesnsenecal@gmail.com>
2022-04-26 13:16:34 -0400

    commit tree b5096db8a39ff446cc099a2127a191158a14682a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLoc) {
+    return Math.abs(42 - pickUpLoc);
+}
+
+distanceFromHqInBlocks(43);
+
+function distanceFromHqInFeet(pickUpLoc) {
+    return distanceFromHqInBlocks(pickUpLoc) * 264;
+}
+
+distanceFromHqInFeet(1);
+
+
+function distanceTravelledInFeet(grab, dropOffLoc) {
+    return Math.abs(grab - dropOffLoc) * 264;
+}
+
+distanceTravelledInFeet();
+
+function calculatesFarePrice(pickUp, dropOff) {
+    if (distanceTravelledInFeet(pickUp, dropOff) < 400) {
+        return 0
+    } else if (distanceTravelledInFeet(pickUp, dropOff) > 400 && distanceTravelledInFeet(pickUp, dropOff) < 2000) {
+        return (distanceTravelledInFeet(pickUp, dropOff) - 400) * .02;
+    } else if (distanceTravelledInFeet(pickUp, dropOff) > 2000 && distanceTravelledInFeet(pickUp, dropOff) <= 2500) {
+        return 25
+    } else if (distanceTravelledInFeet(pickUp, dropOff) > 2500) {
+        return 'cannot travel that far'
+    }
+}
+
+calculatesFarePrice()
